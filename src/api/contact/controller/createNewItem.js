cd6213085a31d55c91320ffa4e607dd9c89ac870
Raihan Ahmad <77282000+raihan2945@users.noreply.github.com>
2024-06-11 22:14:12 +0600

    Update createNewItem.js

--- src/api/contact/controller/createNewItem.js
+++ src/api/contact/controller/createNewItem.js
@@ -4,45 +4,44 @@ const inputSchema = require("@/src/validators/inputValidation");
 const createNewItem = async (req, res, next) => {
   const bodyData = req.body;
 
-  console.log('body data is : ', bodyData)
+  // console.log('body data is : ', bodyData)
 
-  const validationProperties = [
-    { name: "category_id", type: "number", required: false },
-    { name: "first_name", type: "string", required: true },
-    { name: "last_name", type: "string", required: false },
-    { name: "company", type: "string", required: false },
-    { name: "phone", type: "number", required: false },
-    { name: "mobile", type: "mobile", required: false },
-    { name: "email", type: "email", required: false },
-    { name: "dob", type: "string", required: false },
-    { name: "address", type: "string", required: false },
-    { name: "address_2", type: "string", required: false },
-    { name: "notes", type: "string", required: false },
-    { name: "division", type: "string", required: false },
-    { name: "district", type: "string", required: false },
-    { name: "upazila", type: "string", required: false },
-    { name: "union", type: "string", required: false },
-    { name: "tag", type: "string", required: false },
-    { name: "office", type: "string", required: false },
-
-  ];
+  // const validationProperties = [
+  //   { name: "category_id", type: "number", required: false },
+  //   { name: "first_name", type: "string", required: true },
+  //   { name: "last_name", type: "string", required: false },
+  //   { name: "company", type: "string", required: false },
+  //   { name: "phone", type: "number", required: false },
+  //   { name: "mobile", type: "mobile", required: false },
+  //   { name: "email", type: "email", required: false },
+  //   { name: "dob", type: "string", required: false },
+  //   { name: "address", type: "string", required: false },
+  //   { name: "address_2", type: "string", required: false },
+  //   { name: "notes", type: "string", required: false },
+  //   { name: "division", type: "string", required: false },
+  //   { name: "district", type: "string", required: false },
+  //   { name: "upazila", type: "string", required: false },
+  //   { name: "union", type: "string", required: false },
+  //   { name: "tag", type: "string", required: false },
+  //   { name: "office", type: "string", required: false },
+  // ];
 
   //* BUILD INPU T SCHEMA
-  const buildedSchema = inputSchema(validationProperties);
+  // const buildedSchema = inputSchema(validationProperties);
 
   //* : VALIDATE INPUT DATA
-  const { error } = buildedSchema.validate(
-    { ...bodyData },
-    { abortEarly: false }
-  );
+  // const { error } = buildedSchema.validate(
+  //   { ...bodyData },
+  //   { abortEarly: false }
+  // );
 
-  if (error) {
-    return res.status(400).json({
-      error: error.details.map((details) => {
-        return details.message;
-      }),
-    });
-  }
+  // if (error) {
+  //   return res.status(400).json({
+  //     error: error.details.map((details) => {
+  //       return details.message;
+  //     }),
+  //   });
+  // }
 
   try {
     //*:
