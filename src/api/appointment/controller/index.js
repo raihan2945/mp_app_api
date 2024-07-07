f6df9305c33975691e1fbe5742a281a577afe703
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-07 15:35:08 +0600

    feat: adding get single appointment api

--- src/api/appointment/controller/index.js
+++ src/api/appointment/controller/index.js
@@ -2,6 +2,7 @@ const createNew = require("./createNew");
 const getAll = require('./getAll')
 const deleteItem = require('./deleteItem')
 const updateItem = require('./updateItem')
+const getItem = require('./getItem')
 
 
 
@@ -9,5 +10,6 @@ module.exports = {
     createNew,
     getAll,
     deleteItem,
-    updateItem
+    updateItem,
+    getItem
 }
--- src/routes/v1/appointment.js
+++ src/routes/v1/appointment.js
@@ -4,6 +4,7 @@ const { controller: appointmentController } = require("@/src/api/appointment");
 
 router.post("/", appointmentController.createNew);
 router.get("/", appointmentController.getAll);
+router.get("/:id", appointmentController.getItem);
 router.delete("/:id", appointmentController.deleteItem);
 router.patch("/:id", appointmentController.updateItem);
 
