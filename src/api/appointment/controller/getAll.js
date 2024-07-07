bc6ce0654c33ccd66ca1f47127b5f5df66468d24
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-07 18:00:48 +0600

    feat: adding pagination in get all appointment api

--- src/api/appointment/controller/getAll.js
+++ src/api/appointment/controller/getAll.js
@@ -2,9 +2,14 @@ const { Appointment } = require("@/src/model");
 
 
 const getAll = async(req, res, next) => {
-    const data = await Appointment.findAll()
 
-    return res.status(200).json(data)
+    const currentPage = (Number(req.query?.page) - 1) || 0
+    const limit = Number(req.query?.limit) || 10
+
+    const data = await Appointment.findAll({offset: currentPage*limit, limit: limit})
+    const count = await Appointment.count()
+
+    return res.status(200).json({data:data, count: count})
 }
 
 module.exports = getAll
