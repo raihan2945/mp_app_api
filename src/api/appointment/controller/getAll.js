f1c6ddda9c44043711b7e0adc384c89431b40564
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-08 17:01:47 +0600

    feat: add daterange filter in getAll appointment endpoint

--- src/api/appointment/controller/getAll.js
+++ src/api/appointment/controller/getAll.js
@@ -5,6 +5,8 @@ const getAll = async (req, res, next) => {
   const currentPage = Number(req.query?.page) - 1 || 0;
   const limit = Number(req.query?.limit) || 10;
   const search = req.query?.search;
+  const dateStart = req.query?.start;
+  const dateEnd = req.query?.end;
 
   let data = await Appointment.findAll({
     offset: currentPage * limit,
@@ -19,17 +21,38 @@ const getAll = async (req, res, next) => {
       limit: limit,
       where: {
         full_name: {
-            [Op.like]: `%${search}%`
+          [Op.like]: `%${search}%`,
         },
       },
     });
 
     count = await Appointment.count({
-        where: {
-            full_name: {
-                [Op.like]: `%${search}%`
-            },
-          },
+      where: {
+        full_name: {
+          [Op.like]: `%${search}%`,
+        },
+      },
+    });
+  }
+
+  // date picker range
+  if (dateStart && dateEnd) {
+    data = await Appointment.findAll({
+      offset: currentPage * limit,
+      limit: limit,
+      where: {
+        created_at: {
+          [Op.between]: [new Date(dateStart), new Date(dateEnd)],
+        },
+      },
+    });
+
+    count = await Appointment.count({
+      where: {
+        created_at: {
+          [Op.between]: [new Date(dateStart), new Date(dateEnd)],
+        },
+      },
     });
   }
 
